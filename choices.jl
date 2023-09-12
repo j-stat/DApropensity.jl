a1780053b2ef38750d9392955064af6180b51d4b
August Guang <august.guang@gmail.com>
2023-09-12 12:07:46 -0400

    #2. Generated lotto numbers, deferredacceptance input has different params

--- choices.jl
+++ choices.jl
@@ -1,23 +1,30 @@
 using Distributions
+using Random
+using DeferredAcceptance
 
-function choices(n, m, M, k)
-    for i = 1:n
-        schoolRank = schools(m, M)
+function choices(numStudents, numSchools, totalSchools, numRankings)
+    for i = 1:numStudents
+        schoolRank = schools(numSchools, totalSchools)
         println(schoolRank) # for debugging
-        priorityRank = priority(k)
+        priorityRank = priority(numRankings)
         println(priorityRank) # for debugging
     end
     # todo: configure output to go into DA algorithm
 end
 
 # student chooses m schools out of possible M
-function schools(m, M)
-    rand(DiscreteUniform(1, M), m)
+function schools(numSchools, totalSchools)
+    rand(DiscreteUniform(1, totalSchools), numSchools)
 end
 
 # school gives student 1 of its k priority rankings
-function priority(k)
-    rand(DiscreteUniform(1, k))
+function priority(numRankings)
+    rand(DiscreteUniform(1, numRankings))
+end
+
+function lottery(numStudents, numSims)
+    randperm(numStudents)
+    # todo: configure output to go into DA algorithm
 end
 
 choices(10, 3, 5, 8)
