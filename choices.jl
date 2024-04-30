cc8e80f9e1e4a7dd860fc90bc9a43f04f6920b63
Jordan <jordan_lawson@brown.edu>
2024-04-30 15:53:00 -0400

    fixed priority function

---
+++ choices.jl
@@ -0,0 +1,31 @@
+using Distributions
+using Random
+using DeferredAcceptance
+
+function choices(numStudents, numSchools, totalSchools, numRankings)
+    for i = 1:numStudents
+        schoolRank = schools(numSchools, totalSchools)
+        println(schoolRank) # for debugging
+        priorityRank = priority(numRankings, numSchools)
+        println(priorityRank) # for debugging
+    end
+    # todo: configure output to go into DA algorithm
+end
+
+# student chooses m schools out of possible M
+function schools(numSchools, totalSchools)
+    rand(DiscreteUniform(1, totalSchools), numSchools)
+end
+
+# school gives student 1 of its k priority rankings 
+# updated so students get ranking at each school to which they apply
+function priority(numRankings, m)
+    rand(DiscreteUniform(1, numRankings), m)
+end
+
+function lottery(numStudents, numSims)
+    randperm(numStudents)
+    # todo: configure output to go into DA algorithm
+end
+
+choices(10, 3, 5, 8)
