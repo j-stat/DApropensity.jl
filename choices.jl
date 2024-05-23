2cae1d06ccb1e8fd63a7a5d68db2934dff859f33
August Guang <august.guang@gmail.com>
2024-05-23 12:33:34 -0400

    #7, configured school rankings and tiebreaking with lotto numbers

--- choices.jl
+++ choices.jl
@@ -2,43 +2,40 @@ using Distributions
 using Random
 using DeferredAcceptance
 
-function simulate(numTimes, schools)
+function simulate(numTimes, schools, students, capacities)
     schools_tiebroken = singletiebreaking(schools)
     assn, rank = deferredacceptance(students, schools_tiebroken, capacities)
     println(assn)
 end
 
-function choices(numStudents, numSchools, totalSchools, numRankings)
+function choices(numStudents, numSchools, totalSchools, numRankings, maxRank=999)
     students = Array{Int}(undef, numStudents, totalSchools)
-    #schools = Array{Int}(undef, totalSchools, numStudents)
     for i = 1:numStudents
-        schoolRank = schools(numSchools, totalSchools)
-        students[i,:] = schoolRank
-        priorityRank = priority(numRankings, numSchools)
-        #schools[i,:] .= prioritRank
-        println(priorityRank) # for debugging
+        student = studentRanking(numSchools, totalSchools, maxRank)
+        students[i,:] = student
     end
-    # todo: configure output to go into DA algorithm
-    println(students)
+
+    schools = Array{Int}(undef, totalSchools, numStudents)
+    for i = 1:totalSchools
+        priority = priorityRanking(students[:,i], numRankings, maxRank)
+        schools[i,:] = priority
+    end
 end
 
 # student chooses m schools out of possible M
 # no school can have the same ranking, i.e. 2 schools cannot both have ranking 1
-# non chosen schools get ranked at bottom (i.e. 9999)
-function schools(numSchools, totalSchools)
+# non chosen schools get ranked at bottom (i.e. maxRank)
+function studentRanking(numSchools, totalSchools, maxRank)
     shuf = shuffle(1:totalSchools) # to be replaced later with actual distribution
-    replace(x -> x .> numSchools ? 999 : x, shuf)
+    replace(x -> x .> numSchools ? maxRank : x, shuf)
 end
 
 # school gives student 1 of its k priority rankings 
 # updated so students get ranking at each school to which they apply
-function priority(numRankings, m)
-    rand(DiscreteUniform(1, numRankings), m)
-end
-
-function lottery(numStudents, numSims)
-    randperm(numStudents)
-    # todo: configure output to go into DA algorithm
+# only ranks students that have chosen the school
+# can be tied
+function priorityRanking(ranks, numRankings, maxRank)
+    replace(x -> x .!= maxRank ? rand(DiscreteUniform(1, numRankings)) : x, ranks)
 end
 
 choices(10, 3, 5, 8)
