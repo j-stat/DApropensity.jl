7abbf4a0d78cdb0768ec8308fa27a49b8a99c087
August Guang <august.guang@gmail.com>
2024-05-23 12:01:12 -0400

    #7, fixed schools to create school array for deferredacceptance.jl

--- choices.jl
+++ choices.jl
@@ -4,21 +4,30 @@ using DeferredAcceptance
 
 function simulate(numTimes, schools)
     schools_tiebroken = singletiebreaking(schools)
+    assn, rank = deferredacceptance(students, schools_tiebroken, capacities)
+    println(assn)
 end
 
 function choices(numStudents, numSchools, totalSchools, numRankings)
+    students = Array{Int}(undef, numStudents, totalSchools)
+    #schools = Array{Int}(undef, totalSchools, numStudents)
     for i = 1:numStudents
         schoolRank = schools(numSchools, totalSchools)
-        println(schoolRank) # for debugging
+        students[i,:] = schoolRank
         priorityRank = priority(numRankings, numSchools)
+        #schools[i,:] .= prioritRank
         println(priorityRank) # for debugging
     end
     # todo: configure output to go into DA algorithm
+    println(students)
 end
 
 # student chooses m schools out of possible M
+# no school can have the same ranking, i.e. 2 schools cannot both have ranking 1
+# non chosen schools get ranked at bottom (i.e. 9999)
 function schools(numSchools, totalSchools)
-    rand(DiscreteUniform(1, totalSchools), numSchools)
+    shuf = shuffle(1:totalSchools) # to be replaced later with actual distribution
+    replace(x -> x .> numSchools ? 999 : x, shuf)
 end
 
 # school gives student 1 of its k priority rankings 
